7f1c0e7303946559e80d0f22e16734b6bebb3531
Ravis96 <ravis96@o2.pl>
2022-07-18 20:15:26 +0200

    Add some features

--- bukkit/plugin/src/main/java/cc/dreamcode/template/builder/ItemBuilder.java
+++ bukkit/plugin/src/main/java/cc/dreamcode/template/builder/ItemBuilder.java
@@ -14,16 +14,44 @@ public class ItemBuilder {
     private ItemStack itemStack = new ItemStack(Material.AIR);
 
     public ItemBuilder(Material material) {
+        if (material == null) {
+            this.setItemStack(new ItemStack(Material.DIRT));
+            return;
+        }
+
         this.setItemStack(new ItemStack(material));
     }
 
     public ItemBuilder(Material material, int amount) {
+        if (material == null) {
+            this.setItemStack(new ItemStack(Material.DIRT, amount));
+            return;
+        }
+
         this.setItemStack(new ItemStack(material, amount));
     }
 
     public ItemBuilder(ItemStack itemStack) {
+        if (itemStack == null) {
+            this.setItemStack(new ItemStack(Material.DIRT));
+            return;
+        }
+
         this.setItemStack(itemStack);
     }
+
+    public static ItemBuilder of(Material material) {
+        return new ItemBuilder(material);
+    }
+
+    public static ItemBuilder of(Material material, int amount) {
+        return new ItemBuilder(material, amount);
+    }
+
+    public static ItemBuilder of(ItemStack itemStack) {
+        return new ItemBuilder(itemStack);
+    }
+
 
     public ItemBuilder setAmount(int amount) {
         this.itemStack.setAmount(amount);
--- bukkit/plugin/src/main/java/cc/dreamcode/template/features/cache/Pair.java
+++ bukkit/plugin/src/main/java/cc/dreamcode/template/features/cache/Pair.java
@@ -10,4 +10,8 @@ public class Pair<F, S> {
     private final F first;
     private final S second;
 
+    public static <F, S> Pair<F, S> of(F f, S s) {
+        return new Pair<>(f, s);
+    }
+
 }
