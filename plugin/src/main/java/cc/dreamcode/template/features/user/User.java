7e766fa17a9c2c517d0c2f1e001ca9b5a8f57b7b
Ravis96 <ravis96@o2.pl>
2022-09-15 21:30:39 +0200

    Cleanup.

--- plugin/src/main/java/cc/dreamcode/template/features/user/User.java
+++ plugin/src/main/java/cc/dreamcode/template/features/user/User.java
@@ -3,26 +3,19 @@ package cc.dreamcode.template.features.user;
 import cc.dreamcode.template.features.persistence.AbstractDocument;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class User extends AbstractDocument {
 
-    private String name;
+    private String name; // nick of this user
 
     private String value = "default values -- add data";
 
     public UUID getUniqueId() {
         return this.getPath().toUUID();
-    }
-
-    public Optional<Player> getPlayer() {
-        return Optional.ofNullable(Bukkit.getPlayer(this.getUniqueId()));
     }
 
 }
