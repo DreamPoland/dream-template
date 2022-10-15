a478a7a8a9b153ee7a04a9f5e1bfe0ea622277df
Ravis96 <ravis96@spoko.pl>
2022-10-15 14:23:22 +0200

    Cleanup

--- plugin/src/main/java/cc/dreamcode/template/model/user/User.java
+++ plugin/src/main/java/cc/dreamcode/template/model/user/User.java
@@ -1,6 +1,9 @@
 package cc.dreamcode.template.model.user;
 
 import cc.dreamcode.template.storage.document.AbstractDocument;
+import eu.okaeri.configs.annotation.NameModifier;
+import eu.okaeri.configs.annotation.NameStrategy;
+import eu.okaeri.configs.annotation.Names;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,6 +11,7 @@ import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
+@Names(strategy = NameStrategy.HYPHEN_CASE, modifier = NameModifier.TO_LOWER_CASE)
 public class User extends AbstractDocument {
 
     private String name;
