5f3c1f245c23df16c354300e8918408541fb7048
Ravis96 <ravis96@spoko.pl>
2023-03-12 20:29:53 +0100

    Improve example-user-controller by tasker

--- bukkit/src/main/java/cc/dreamcode/template/controller/ExampleUserController.java
+++ bukkit/src/main/java/cc/dreamcode/template/controller/ExampleUserController.java
@@ -4,6 +4,7 @@ import cc.dreamcode.template.BukkitTemplatePlugin;
 import cc.dreamcode.template.user.User;
 import cc.dreamcode.template.user.UserRepository;
 import eu.okaeri.injector.annotation.Inject;
+import eu.okaeri.tasker.core.Tasker;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,16 +18,25 @@ public class ExampleUserController implements Listener {
 
     private @Inject BukkitTemplatePlugin bukkitTemplatePlugin;
     private @Inject UserRepository userRepository;
+    private @Inject Tasker tasker;
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         final Player player = e.getPlayer();
-        final User user = this.userRepository.findOrCreateByPlayerFuture(player).join(); // completable-future get user
 
-        user.setName(player.getName()); // example setter (only for example)
-        this.bukkitTemplatePlugin.runAsync(user::save); // save after changes (async)
-
-        player.sendMessage("hi, " + user.getName()); // send message after save
+        // 5 sync task (1 task = 1 tick)
+        this.tasker.newChain()
+                .async(() -> this.userRepository.findOrCreateByHumanEntity(player)) // async create user
+                .acceptSync(user -> {
+                    user.setName(player.getName()); // example setter (only for example)
+                })
+                .acceptAsync(user -> {
+                    user.save(); // save after changes (async))
+                })
+                .acceptSync(user -> {
+                    player.sendMessage("hi, " + user.getName()); // send message after save
+                })
+                .execute();
     }
 
 }
