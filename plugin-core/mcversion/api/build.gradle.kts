d41af09873c0ea65d0ef8503f0a716ed7aaf251e
Ravis96 <ravis96@spoko.pl>
2024-03-23 12:37:13 +0100

    Fix dependencies in mc-version modules, Bump dependencies

--- plugin-core/mcversion/api/build.gradle.kts
+++ plugin-core/mcversion/api/build.gradle.kts
@@ -4,7 +4,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 
     compileOnly("com.github.cryptomorin:XSeries:9.9.0")
 }
