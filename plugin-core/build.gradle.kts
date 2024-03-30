ed6c7a9d92141aa24d4e1886c3ffb25bedc48409
Ravis96 <ravis96@spoko.pl>
2024-03-30 20:01:51 +0100

    Bump dependencies

--- plugin-core/build.gradle.kts
+++ plugin-core/build.gradle.kts
@@ -15,11 +15,11 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 
     // -- dream-platform --
-    implementation("cc.dreamcode.platform:core:1.10.4")
-    implementation("cc.dreamcode.platform:bukkit:1.10.4")
-    implementation("cc.dreamcode.platform:bukkit-config:1.10.4")
-    implementation("cc.dreamcode.platform:dream-command:1.10.4")
-    implementation("cc.dreamcode.platform:persistence:1.10.4")
+    implementation("cc.dreamcode.platform:core:1.10.5")
+    implementation("cc.dreamcode.platform:bukkit:1.10.5")
+    implementation("cc.dreamcode.platform:bukkit-config:1.10.5")
+    implementation("cc.dreamcode.platform:dream-command:1.10.5")
+    implementation("cc.dreamcode.platform:persistence:1.10.5")
 
     // -- dream-utilities --
     implementation("cc.dreamcode:utilities:1.3.0")
@@ -33,8 +33,8 @@ dependencies {
     implementation("cc.dreamcode.notice:bukkit-adventure-serializer:1.4.9")
 
     // -- dream-command --
-    implementation("cc.dreamcode.command:core:2.0-beta.3")
-    implementation("cc.dreamcode.command:bukkit:2.0-beta.3")
+    implementation("cc.dreamcode.command:core:2.0-beta.4")
+    implementation("cc.dreamcode.command:bukkit:2.0-beta.4")
 
     // -- dream-menu --
     implementation("cc.dreamcode.menu:core:1.2.7")
