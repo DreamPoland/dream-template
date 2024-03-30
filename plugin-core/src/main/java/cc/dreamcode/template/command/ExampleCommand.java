9c3f89c8dd4150964fac6e5953595c1325b499b4
Ravis96 <ravis96@spoko.pl>
2024-03-30 16:28:48 +0100

    Improve example-command

--- plugin-core/src/main/java/cc/dreamcode/template/command/ExampleCommand.java
+++ plugin-core/src/main/java/cc/dreamcode/template/command/ExampleCommand.java
@@ -1,12 +1,14 @@
 package cc.dreamcode.template.command;
 
 import cc.dreamcode.command.CommandBase;
+import cc.dreamcode.command.DreamSender;
 import cc.dreamcode.command.annotation.Arg;
 import cc.dreamcode.command.annotation.Command;
 import cc.dreamcode.command.annotation.Completion;
 import cc.dreamcode.command.annotation.Executor;
 import cc.dreamcode.command.annotation.OptArg;
-import cc.dreamcode.command.bukkit.BukkitSender;
+import cc.dreamcode.command.annotation.Sender;
+import org.bukkit.entity.Player;
 
 import java.time.Duration;
 
@@ -14,11 +16,12 @@ import java.time.Duration;
 public class ExampleCommand implements CommandBase {
 
     @Executor(description = "Przykladowa komenda.")
+    @Sender(type = DreamSender.Type.CLIENT)
     @Completion(arg = "bro", value = "@allplayers")
     @Completion(arg = "bro2", value = {"1", "2", "3"})
-    public void example(BukkitSender bukkitSender, @Arg(name = "bro") String bro, @Arg(name = "bro2") Duration bro2, @OptArg(name = "bro3") String bro3) {
-        bukkitSender.getHandler().sendMessage("hello, " + bro);
-        bukkitSender.getHandler().sendMessage("hello 2, " + bro2);
-        bukkitSender.getHandler().sendMessage("hello 3, " + bro3);
+    public void example(Player player, @Arg(name = "bro") Player bro, @Arg(name = "bro2") Duration bro2, @OptArg(name = "bro3") String bro3) {
+        player.sendMessage("hello, " + bro.getName());
+        player.sendMessage("hello 2, " + bro2);
+        player.sendMessage("hello 3, " + bro3);
     }
 }
