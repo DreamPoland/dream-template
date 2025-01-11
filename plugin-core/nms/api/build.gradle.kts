5d40f257057acfcee4d326a603b11a79f558e362
Ravis96 <ravis96@spoko.pl>
2025-01-11 18:31:58 +0100

    Change spigot implementation to spigot-api

--- plugin-core/nms/api/build.gradle.kts
+++ plugin-core/nms/api/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 
     // -- dream-utilities --
     implementation("cc.dreamcode:utilities:1.5.1")
